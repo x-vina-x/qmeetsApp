c862484dcc82395ea81ed71cc87eba4b5063e130
alvina <chen.b.alvina@gmail.com>
2021-03-18 21:21:44 -0500

    App foundations

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import { StyleSheet, Text, View } from 'react-native';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -9,7 +9,9 @@ import Navigation from './navigation';
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+    <View style={styles.container}>
+      <Text>To share a photo from your phone with a friend, just press the button below!</Text>
+    </View>
   if (!isLoadingComplete) {
     return null;
   } else {
@@ -21,3 +23,12 @@ export default function App() {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
